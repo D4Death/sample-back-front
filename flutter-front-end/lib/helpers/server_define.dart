2451472191805601e48a361ccd8e20061de4b5d6
Cuong Trieu <cuong.trieu2309@gmail.com>
2022-11-23 00:00:14 +0700

    INIT flutter

---
+++ flutter-front-end/lib/helpers/server_define.dart
@@ -0,0 +1,8 @@
+class ServerUri {
+  static const FINO_API = 'http://localhost:8090';
+
+  static const FINO_SOCKET = 'http://127.0.0.1:3000';
+
+  static const NEWS_API = 'https://newsdata.io';
+  static const NEWS_DATA_API_KEY = 'pub_104303f4636afc731fdf65f0a67df45df53';
+}
